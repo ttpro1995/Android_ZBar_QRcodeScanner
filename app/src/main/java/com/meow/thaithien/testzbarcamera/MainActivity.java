19ed6b1fa46ee312edfad2ca834c397f074fc9dd
unknown <tthien@apcs.vn>
2015-07-21 22:45:56 +0700

    init

---
+++ app/src/main/java/com/meow/thaithien/testzbarcamera/MainActivity.java
@@ -0,0 +1,103 @@
+package com.meow.thaithien.testzbarcamera;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import me.dm7.barcodescanner.zbar.Result;
+import me.dm7.barcodescanner.zbar.ZBarScannerView;
+
+
+public class MainActivity extends Activity {
+
+    String TAG = "Zbar";
+    FrameLayout frameLayout;// a placeholder for code scanner preview
+    ZBarScannerView myScannerView;// scanner view which we will add into frameLayout
+    ZBarScannerView.ResultHandler resultHandler;//result handler for myScannerView
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        frameLayout = (FrameLayout) findViewById(R.id.PlaceHolderFram);
+
+        //TODO: init the result handler
+        resultHandler = new ZBarScannerView.ResultHandler() {
+            @Override
+            public void handleResult(Result result) {
+                // Do something with the result here
+                Log.v(TAG, result.getContents()); // Prints scan results
+                Log.v(TAG, result.getBarcodeFormat().getName()); // Prints the scan format (qrcode, pdf417 etc.)
+
+                //Result show here
+                ShowResultDialog(result.getContents());
+            }
+        };
+
+        //TODO: init view and add view to layout
+        myScannerView = new ZBarScannerView(MainActivity.this);
+        frameLayout.addView(myScannerView);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //TODO: SET Result Handle Here
+        myScannerView.setResultHandler(resultHandler);
+        //TODO Start camera
+        myScannerView.startCamera();
+        //TODO Set flash and autofocus
+        myScannerView.setFlash(true);
+        myScannerView.setAutoFocus(true);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        myScannerView.stopCamera();           // Stop camera on pause
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void ShowResultDialog(String msg){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage(msg);
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                MainActivity.this.onResume();
+            }
+        });
+       Dialog dialog = builder.create();
+        dialog.show();
+    }
+
+}
